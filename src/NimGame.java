72a37570f19fe8cd6b01eb92b93a344f251154d6
AceXIE <1048345175@qq.com>
2016-09-20 14:53:38 +0800

    https://leetcode.com/problems/nim-game/

---
+++ src/NimGame.java
@@ -0,0 +1,11 @@
+
+public class NimGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public static boolean canWinNim(int n) {
+        return n%4 != 0;
+    }
+}
