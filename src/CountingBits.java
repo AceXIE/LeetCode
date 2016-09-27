1e851d7135d5478ca74fdda899f5a5996220d65e
AceXIE <1048345175@qq.com>
2016-09-27 14:03:55 +0800

    https://leetcode.com/problems/counting-bits/

---
+++ src/CountingBits.java
@@ -0,0 +1,20 @@
+
+public class CountingBits {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] ans = countBits(5);
+		for (int i : ans)
+			System.out.print(i + ",");
+	}
+	public static int[] countBits(int num) {
+		int[] ans = new int[num  +1];
+		ans[0] = 0;
+		for (int i = 1; i <= num; i++) {
+			ans[i] = ans[i&(i - 1)] + 1;
+		}
+		return ans;
+    }
+}
